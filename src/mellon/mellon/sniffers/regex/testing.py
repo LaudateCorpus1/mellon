a053ed1418cd49cae4658fe2f108da78fd84f55d
David Davis <davisd50@gmail.com>
2017-01-24 11:31:48 -0500

    remove unused testing layer

--- src/mellon/mellon/sniffers/regex/testing.py
+++
@@ -1,8 +0,0 @@
-import mellon
-from sparc.testing.testlayer import SparcZCMLFileLayer
-
-
-class MellonSniffersRegexLayer(SparcZCMLFileLayer):
-    pass
-
-MELLON_SNIFFERS_REGEX_LAYER = MellonSniffersRegexLayer(mellon)
