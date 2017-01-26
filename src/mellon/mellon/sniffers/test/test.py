d0fbb0af09aa24efdb329740b0ce7cee85432eca
David Davis <davisd50@gmail.com>
2017-01-26 14:43:22 -0500

    update to new interface requirements

--- src/mellon/mellon/sniffers/test/test.py
+++ src/mellon/mellon/sniffers/test/test.py
@@ -5,20 +5,22 @@ import mellon
 CALLED_BYTES = False
 CALLED_UNICODE = False
 
+def reset_test_sniffer():
+    global CALLED_BYTES, CALLED_UNICODE
+    CALLED_BYTES = CALLED_UNICODE = False
+
 @interface.implementer(mellon.ISecretSniffer)
 @component.adapter(mellon.IBytesSnippet)
 class TestBytesSecretSniffer(object):
     def __init__(self, context):
         self.context = context
     
-    def __call__(self):
+    def __iter__(self):
         global CALLED_BYTES
-        return_ = None
         if not CALLED_BYTES:
-            return_ = component.createObject(u"mellon.secret", 
+            yield component.createObject(u"mellon.secret", 
                             name=u"This is a test bytes secret", parent=self.context)
             CALLED_BYTES = True
-        return return_
 
 @interface.implementer(mellon.ISecretSniffer)
 @component.adapter(mellon.IUnicodeSnippet)
@@ -26,11 +28,9 @@ class TestUnicodeSecretSniffer(object):
     def __init__(self, context):
         self.context = context
     
-    def __call__(self):
+    def __iter__(self):
         global CALLED_UNICODE
-        return_ = None
         if not CALLED_UNICODE:
-            return_ = component.createObject(u"mellon.secret", 
+            yield component.createObject(u"mellon.secret", 
                             name=u"This is a test unicode secret", parent=self.context)
             CALLED_UNICODE = True
-        return return_
