1fa025d0d8c098503d4f2761b04eb015b0b1d2c8
David Davis <davisd50@gmail.com>
2017-01-26 15:49:32 -0500

    add monkey patch to db engine to insure sqlite foreign key checks are enabled.

--- src/mellon_plugin.reporter.sqlalchemy/mellon_plugin/reporter/sqlalchemy/orm/testing.py
+++ src/mellon_plugin.reporter.sqlalchemy/mellon_plugin/reporter/sqlalchemy/orm/testing.py
@@ -1,9 +1,20 @@
 from mellon.testing import MellonApplicationRuntimeLayer
 from mellon.reporters.memory.memory import reset_report
+from sqlalchemy import create_engine, event
 import mellon_plugin.reporter.sqlalchemy.orm
 from .db import get_session
 
 class MellonOrmRuntimeReporterLayer(MellonApplicationRuntimeLayer):
+    
+    def create_engine(self):
+        #http://stackoverflow.com/questions/2614984/sqlite-sqlalchemy-how-to-enforce-foreign-keys
+        def _fk_pragma_on_connect(dbapi_con, con_record):
+            dbapi_con.execute('pragma foreign_keys=ON')
+        engine = create_engine('sqlite:///:memory:', echo=self.debug)
+        event.listen(engine, 'connect', _fk_pragma_on_connect)
+        return engine
+    
+
     def setUp(self):
         self.config = \
             {
@@ -23,6 +34,9 @@ class MellonOrmRuntimeReporterLayer(MellonApplicationRuntimeLayer):
                  ]
             }
         super(MellonOrmRuntimeReporterLayer, self).setUp()
+        # We're gonna monkey-patch the default engine to make sure FK checks operate
+        self.engine = self.create_engine()
+        mellon_plugin.reporter.sqlalchemy.orm.db._Engine = self.engine
         self.session = get_session()
     
     def tearDown(self):
