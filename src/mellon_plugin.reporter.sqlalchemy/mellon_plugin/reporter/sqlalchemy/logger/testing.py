6a5613509325fb825550187d30de4a890d044dcb
David Davis <davisd50@gmail.com>
2017-01-26 14:49:18 -0500

    add test layer

---
+++ src/mellon_plugin.reporter.sqlalchemy/mellon_plugin/reporter/sqlalchemy/logger/testing.py
@@ -0,0 +1,39 @@
+from zope import component
+from mellon.testing import MellonApplicationRuntimeLayer
+from mellon.reporters.memory.memory import reset_report
+import mellon_plugin.reporter.sqlalchemy.logger
+
+from .interfaces import IDBReporter
+
+class MellonLoggerRuntimeReporterLayer(MellonApplicationRuntimeLayer):
+    def setUp(self):
+        self.config = \
+            {
+             'SQLAlchemyLoggerReporter':
+                {'SQLAlchemyReporter':
+                    {'SQLAlchemyEngine':
+                        {'dsn': 'sqlite:///:memory:'},
+                     }
+                 },
+             'MellonFileProviderFactory':
+                {'name': 'mellon.factories.test.file_provider_for_testing'},
+             'ZCMLConfiguration':
+                [
+                    {'package':'mellon.sniffers.test'},
+                    {'package':'mellon.factories.test'},
+                    {'package':'mellon_plugin.reporter.sqlalchemy.logger'}
+                 ]
+            }
+        super(MellonLoggerRuntimeReporterLayer, self).setUp()
+        self.reporter = component.getUtility(IDBReporter)
+    
+    def tearDown(self):
+        reset_report()
+        super(MellonLoggerRuntimeReporterLayer, self).tearDown()
+MELLON_SA_LOGGER_REPORTER_RUNTIME_LAYER = MellonLoggerRuntimeReporterLayer(mellon_plugin.reporter.sqlalchemy)
+
+class MellonLoggerExecutedReporterLayer(MellonLoggerRuntimeReporterLayer):
+    def setUp(self):
+        super(MellonLoggerExecutedReporterLayer, self).setUp()
+        self.app.go()
+MELLON_SA_LOGGER_REPORTER_EXECUTED_LAYER = MellonLoggerExecutedReporterLayer(mellon_plugin.reporter.sqlalchemy.logger)
