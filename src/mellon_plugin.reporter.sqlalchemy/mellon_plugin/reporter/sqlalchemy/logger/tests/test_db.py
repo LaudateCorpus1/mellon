1a20808130ddf08d34b57b5f76646f7909e14365
David Davis <davisd50@gmail.com>
2017-01-26 14:49:54 -0500

    update to leverage new testing facilities

--- src/mellon_plugin.reporter.sqlalchemy/mellon_plugin/reporter/sqlalchemy/logger/tests/test_db.py
+++ src/mellon_plugin.reporter.sqlalchemy/mellon_plugin/reporter/sqlalchemy/logger/tests/test_db.py
@@ -1,60 +1,18 @@
 import os.path
 import unittest
-from zope import component
 import zope.testrunner
 from sparc.testing.fixture import test_suite_mixin
-from mellon.testing import MellonRuntimeLayerMixin
-from mellon.reporters.memory import memory
-from mellon.sniffers.regex.tests.test_regex import MellonSnifferRegExTestCase
-from mellon_plugin.reporter.sqlalchemy.logger import IDBReporter
 from sqlalchemy import sql
 
-base_path = os.path.dirname(__file__)
-
-import mellon_plugin.reporter.sqlalchemy.logger
-class MellonSAReporterLayer(MellonRuntimeLayerMixin):
-    pass
-MELLON_SA_LOGGER_REPORTER_RUNTIME_LAYER = MellonSAReporterLayer(mellon_plugin.reporter.sqlalchemy.logger)
+from ..testing import MELLON_SA_LOGGER_REPORTER_EXECUTED_LAYER
 
 class MellonSAReporterTestCase(unittest.TestCase):
-    layer = MELLON_SA_LOGGER_REPORTER_RUNTIME_LAYER
-    sm = component.getSiteManager()
-    report = memory.report
-    
-    def __init__(self, *args, **kwargs):
-        super(MellonSAReporterTestCase, self).__init__(*args, **kwargs)
-        config = MellonSnifferRegExTestCase.config.copy()
-        config.update(
-                {
-                 'SQLAlchemyLoggerReporter':
-                    {'SQLAlchemyReporter':
-                        {'SQLAlchemyEngine':
-                            {'dsn': 'sqlite:///:memory:'},
-                         }
-                     }
-                }
-            )
-        self.layer.config.update(config)
-        self.layer.config['ZCMLConfiguration'].extend(
-                        [
-                             {'package':'mellon.sniffers.regex'},
-                             {'package':'mellon.factories.filesystem'},
-                             {'package':'mellon_plugin.reporter.sqlalchemy.logger'}
-                        ])
-        self.layer.verbose = False
-        self.layer.debug = False
-        
-        self.app = self.layer.create_registered_app()
-        self.app.configure()
-        self.reporter = component.getUtility(IDBReporter)
-    
-    def tearDown(self):
-        memory.reset_report()
-    
+    layer = MELLON_SA_LOGGER_REPORTER_EXECUTED_LAYER
+
     def get_rows(self):
-        connection = self.reporter.engine.connect()
+        connection = self.layer.reporter.engine.connect()
         _return = []
-        result = connection.execute(sql.select([self.reporter.tables['secrets']]))
+        result = connection.execute(sql.select([self.layer.reporter.tables['secrets']]))
         for row in result:
             _return.append(row)
         result.close()
@@ -62,17 +20,14 @@ class MellonSAReporterTestCase(unittest.TestCase):
         return _return
 
     def test_reporter(self):
-        self.assertFalse(self.reporter.initialized())
-        self.reporter.update_schema()
-        self.assertTrue(self.reporter.initialized())
-        
-        self.assertEquals(self.get_rows(), [])
-        self.app.go()
-        self.assertEquals(len(self.get_rows()), 4)
+        #self.assertFalse(self.layer.reporter.initialized())
+        #self.layer.reporter.update_schema()
+        self.assertTrue(self.layer.reporter.initialized())
+        self.assertEquals(len(self.get_rows()), 2)
         
 
 class test_suite(test_suite_mixin):
-    layer = MELLON_SA_LOGGER_REPORTER_RUNTIME_LAYER
+    layer = MELLON_SA_LOGGER_REPORTER_EXECUTED_LAYER
     package = 'mellon_plugin.reporter.sqlalchemy.logger'
     module = 'db'
     
