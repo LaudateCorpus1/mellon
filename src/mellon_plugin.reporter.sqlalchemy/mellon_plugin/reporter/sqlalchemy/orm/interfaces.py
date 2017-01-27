b9ca0c36ecfe30482f21bbc7d9216a51a165facf
David Davis <davisd50@gmail.com>
2017-01-27 09:40:53 -0500

    add ISAModel based interfaces

--- src/mellon_plugin.reporter.sqlalchemy/mellon_plugin/reporter/sqlalchemy/orm/interfaces.py
+++ src/mellon_plugin.reporter.sqlalchemy/mellon_plugin/reporter/sqlalchemy/orm/interfaces.py
@@ -1,25 +1,46 @@
 from zope import interface
 
+# BASES
+# we separate the ORM data model vs the SA implementation-specific marker
 class IORMModel(interface.Interface):
     """Base class for ORM models"""
 
+class ISAModel(IORMModel):
+    """Base class for all SQLAlchemy ORM models"""
+
+# AUTHORIZATION CONTEXT
 class IORMAuthorizationContext(IORMModel):
     id = interface.Attribute("Text unique identity")
     name = interface.Attribute("String representation of unique authorization context")
     description = interface.Attribute("Text context describer")
+    
+class ISAAuthorizationContext(ISAModel, IORMAuthorizationContext):
+    """A SA ORM authorization context"""
 
+# MELLON FILE
 class IORMMellonFile(IORMModel):
     name = interface.Attribute("String locatable identity of file")
     authorization_context_id = interface.Attribute("Valid IORMAuthorizationContext.id reference")
+    
+class ISAMellonFile(ISAModel, IORMMellonFile):
+    """A SA ORM mellon file"""
 
+# SNIPPET
 class IORMSnippet(IORMModel):
     id = interface.Attribute("String unique reference id for snippet")
     name = interface.Attribute("String internal snippet location information")
     data_blob = interface.Attribute("A Python byte data sequence")
     data_text = interface.Attribute("A Python unicode data sequence")
     mellon_file_name = interface.Attribute("Valid IORMMellonFile.name reference")
+    
+class ISASnippet(ISAModel, IORMSnippet):
+    """A SA ORM snippet"""
 
+# SECRET
 class IORMSecret(IORMModel):
     id = interface.Attribute("String identifier that uniquely identifies the locatable secret among other secrets")
     name = interface.Attribute("String details of the secret and/or how it was found")
     snippet_id = interface.Attribute("valid IORMSnippet.id reference")
+    
+class ISASecret(ISAModel, IORMSecret):
+    """A SA ORM secret"""
