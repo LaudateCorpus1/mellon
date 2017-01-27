c4d6210bae1141fa17a1474cac802f229d4e584b
David Davis <davisd50@gmail.com>
2017-01-27 09:43:05 -0500

    test models against uniqueness

--- src/mellon_plugin.reporter.sqlalchemy/mellon_plugin/reporter/sqlalchemy/orm/tests/test_db.py
+++ src/mellon_plugin.reporter.sqlalchemy/mellon_plugin/reporter/sqlalchemy/orm/tests/test_db.py
@@ -3,19 +3,35 @@ import unittest
 import zope.testrunner
 from sparc.testing.fixture import test_suite_mixin
 
+from zope import component
+from mellon.reporters.memory.memory import report as memory_report
 from ..testing import MELLON_SA_ORM_REPORTER_EXECUTED_LAYER
 from .. import models
+from .. import interfaces as i
 
 
 class MellonOrmReporterTestCase(unittest.TestCase):
     layer = MELLON_SA_ORM_REPORTER_EXECUTED_LAYER
     
-    def setUp(self):
-        super(MellonOrmReporterTestCase, self).setUp()
+    def tearDown(self):
+        self.layer.session.rollback()
     
     def test_reporter(self):
         # now check orm
         self.assertEquals(len(self.layer.session.query(models.Secret).all()), 2)
+    
+    def test_unique_entries(self):
+        #verify current DN contents
+        self.assertEquals(len(self.layer.session.query(models.MellonFile).all()), 2)
+        mfile = memory_report[0].__parent__.__parent__
+        # create new non-unique model and then merge/check
+        mfile_model_new = component.createObject(u"mellon_plugin.reporter.sqlalchemy.orm.model", mfile)
+        self.layer.session.merge(mfile_model_new)
+        self.assertEquals(len(self.layer.session.query(models.MellonFile).all()), 2)
+        # create new unique model and then merge/check
+        mfile_model_unique = models.MellonFile(name="new unique file")
+        self.layer.session.merge(mfile_model_unique)
+        self.assertEquals(len(self.layer.session.query(models.MellonFile).all()), 3)
 
 class test_suite(test_suite_mixin):
     layer = MELLON_SA_ORM_REPORTER_EXECUTED_LAYER
