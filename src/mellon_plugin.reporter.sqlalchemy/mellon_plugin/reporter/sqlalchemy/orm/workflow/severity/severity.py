a7905b41ec8bfd439683445fe04a0ff4c66ac50a
David Davis <davisd50@gmail.com>
2017-01-31 20:52:07 -0500

    remove references to zope.schema

--- src/mellon_plugin.reporter.sqlalchemy/mellon_plugin/reporter/sqlalchemy/orm/workflow/severity/severity.py
+++ src/mellon_plugin.reporter.sqlalchemy/mellon_plugin/reporter/sqlalchemy/orm/workflow/severity/severity.py
@@ -1,7 +1,5 @@
 from zope import component
 from zope import interface
-from zope.schema.interfaces import IVocabularyFactory
-from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 
 from mellon.mellon import get_registered_app
 from .interfaces import IConfiguredAssignablesSeverities
@@ -13,9 +11,3 @@ class ConfiguredAssignablesSeverities(object):
         terms = m['vgetter'].get('MellonWorkflowSecretAssignableSeverities')
         for t in terms:
             yield (t['value'], t['token'], t['description'])
-
-def configuredAssignablesSeveritiesVocabulary():
-    statuses = component.getUtility(IConfiguredAssignablesSeverities)
-    return SimpleVocabulary([SimpleTerm(s[0], token=s[1], title=s[2]) \
-                                                    for s in statuses.items()])
-interface.alsoProvides(configuredAssignablesSeveritiesVocabulary, IVocabularyFactory)
--- src/mellon_plugin.reporter.sqlalchemy/mellon_plugin/reporter/sqlalchemy/orm/workflow/status/status.py
+++ src/mellon_plugin.reporter.sqlalchemy/mellon_plugin/reporter/sqlalchemy/orm/workflow/status/status.py
@@ -1,7 +1,5 @@
 from zope import component
 from zope import interface
-from zope.schema.interfaces import IVocabularyFactory
-from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 
 from mellon.mellon import get_registered_app
 from .interfaces import IConfiguredAssignablesStatuses
@@ -13,9 +11,3 @@ class ConfiguredAssignablesStatuses(object):
         terms = m['vgetter'].get('MellonWorkflowSecretAssignableStatuses')
         for t in terms:
             yield (t['value'], t['token'], t['description'])
-
-def configuredAssignablesStatusesVocabulary():
-    statuses = component.getUtility(IConfiguredAssignablesStatuses)
-    return SimpleVocabulary([SimpleTerm(s[0], token=s[1], title=s[2]) \
-                                                    for s in statuses.items()])
-interface.alsoProvides(ConfiguredAssignablesStatuses, IVocabularyFactory)
