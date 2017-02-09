f22082792a6a6f04b82f3463b79e40bd33fac632
David Davis <davisd50@gmail.com>
2017-02-09 13:58:51 -0500

    add logging to sa util subscriptions to help indicate problem conditions.

--- src/mellon_api/mellon_api/sa/subscribers/bind_sa_to_app.py
+++ src/mellon_api/mellon_api/sa/subscribers/bind_sa_to_app.py
@@ -20,6 +20,8 @@ def create_and_register_sa_utils(app, event):
         interface.alsoProvides(engine, ISAEngine)
         sm.registerUtility(engine, ISAEngine)
         logger.debug("registered ISAEngine based on mellon_api yaml config for SQLAlchemyORMReporter key")
+    else:
+        logger.debug("ISAEngine engine already available, no need to create and register a new one.")
     
     if not sm.queryUtility(ISASession):
         session_factory = sessionmaker(bind=engine)
@@ -27,6 +29,8 @@ def create_and_register_sa_utils(app, event):
         interface.alsoProvides(Session, ISASession)
         sm.registerUtility(Session, ISASession) # call component.getUtility(ISASession) to get thread-local session
         logger.debug("registered scoped ISASession session utility based")
+    else:
+        logger.debug("ISASession engine already available, no need to create and register a new one.")
 
 @component.adapter(ISAEngine, IRegistrationEvent)
 def create_models(engine, event):
@@ -44,4 +48,6 @@ def commit_request(session, event):
             if not exception:
                 component.getUtility(ISASession).commit()
             component.getUtility(ISASession).remove()
+        else:
+            logger.warn("expected to find a ISASession in order to commit and remove session data, but could not.  The ISASession was probably removed out of band.")
     _test_commit_request_registrations = True
