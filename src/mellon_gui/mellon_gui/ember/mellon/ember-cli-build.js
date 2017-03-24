e08aa26bf248fe561f5911df3598357afa89f643
David Davis <davisd50@gmail.com>
2017-03-24 10:50:03 -0400

    spot checkin for main merge

--- src/mellon_gui/mellon_gui/ember/mellon/ember-cli-build.js
+++ src/mellon_gui/mellon_gui/ember/mellon/ember-cli-build.js
@@ -19,9 +19,35 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-  app.import('bower_components/bootstrap/dist/css/bootstrap.css');
-  app.import('bower_components/bootstrap/dist/css/bootstrap-theme.css');
+  
+  <!-- Core CSS - Include with every page -->
+  <!-- SB Admin CSS - Include with every page -->
+  //app.import('bower_components/bootstrap/dist/css/bootstrap-theme.css');
+  app.import('vendor/sb-admin-v2/css/bootstrap.min.css');
+  <!-- Page-Level Plugin CSS - Tables -->
+  app.import('vendor/sb-admin-v2/css/plugins/dataTables/dataTables.bootstrap.css');
+  <!-- SB Admin CSS - Include with every page -->
+  app.import('vendor/sb-admin-v2/css/sb-admin.css');
+  
+  
+
+  <!-- Core Scripts - Include with every page -->
   app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+  //app.import('vendor/sb-admin-v2/js/plugins/metisMenu/jquery.metisMenu.js');
+
+  <!-- Page-Level Plugin Scripts - Tables -->
+  app.import('vendor/sb-admin-v2/js/plugins/dataTables/jquery.dataTables.js');
+  app.import('vendor/sb-admin-v2/js/plugins/dataTables/dataTables.bootstrap.js');
+
+  <!-- SB Admin Scripts - Include with every page -->
+  app.import('vendor/sb-admin-v2/js/sb-admin.js');
+  
+  <!-- Page-Level Plugin Scripts - Dashboard -->
+  //app.import('bower_components/raphael/raphael.min.js');
+  //app.import('bower_components/morris.js/morris.js');
+
+  <!-- Page-Level Demo Scripts - Dashboard - Use for reference -->
+  //app.import('vendor/sb-admin-v2/js/demo/dashboard-demo.js');
 
   return app.toTree();
 };
